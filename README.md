bcc5b544b15224ef956e3ad1941c78ef84d84dfb
TimEmpey <105009789+TimEmpey@users.noreply.github.com>
2022-11-25 11:22:47 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# _Template_
+
+#### By _**Tim Empey**_
+
+#### _Quicken file creation process_
+
+## Technologies Used
+
+* _JavaScript_
+* _HTML_
+* _CSS_
+* _Bootstrap_
+*_Webpack_
+*_Jest_
+*_Babel_
+*_ES Lint_
+
+## Description
+
+_Template repo to streamline the file setup process for JS webpages with all files set up and ready for use_
+
+## Setup/Installation Requirements
+
+* _Installing all packages with $ npm install._
+* _Building the project using webpack with $ npm run build_
+* _Starting a development server with $ npm run start_
+* _Linting JS files in the src folder with $ npm run lint_
+* _Running tests with Jest using $ npm run test_
+
+## Known Bugs
+
+* _No known bugs_
+
+## License
+
+_[MIT](https://en.wikipedia.org/wiki/MIT_License)_ 
+
+Copyright (c) _2022_ _Timothy Empey_
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {               
+    contentBase: './dist'    
+  },
+  plugins: [
+    new ESLintPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'TEMPLATE',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(gif|png|avif|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
+      
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
