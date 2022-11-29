4b3027ee36077a07107b1e94b4563056dbfea12d
Tim Empey <timothyempey@gmail.com>
2022-11-29 11:26:22 -0800

    add basic content for services + turn responsive

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import './css/services.css';
 import './css/navbar.css';
 import './parallax/parallax.css';
 import './parallax/parallax.js';
